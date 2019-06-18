0b1dc4272d2a960029e83535b0efc9480a1665ea
wangtaocn <WT36017@163.com>
2019-06-18 16:15:43 +0800

    提交代码

---
+++ scripts/test_login.py
@@ -0,0 +1,24 @@
+class TestLogin:
+
+    def test_login1(self):
+        assert 1
+
+    def test_login2(self):
+        assert 0
+
+    def test_login3(self):
+        assert 0
+
+    def test_login4(self):
+        assert 1
+    def test_login5(self):
+        assert 1
+
+    def test_login6(self):
+        assert 0
+
+    def test_login7(self):
+        assert 0
+
+    def test_login8(self):
+        assert 1
